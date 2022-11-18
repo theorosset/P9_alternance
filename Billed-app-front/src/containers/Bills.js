a1e3347073e21a3bb6e811d826599a4be872d67d
Rosset <theo.rosset@outlook.fr>
2022-11-18 12:13:57 +0100

    fix(login): change querySelector params for get good input

--- Billed-app-front/src/containers/Bills.js
+++ Billed-app-front/src/containers/Bills.js
@@ -7,13 +7,21 @@ export default class {
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
+
     const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
-    if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
+
+    if (buttonNewBill) {
+      buttonNewBill.addEventListener('click', this.handleClickNewBill) 
+    }
+
     const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
-    if (iconEye) iconEye.forEach(icon => {
-      icon.addEventListener('click', () => this.handleClickIconEye(icon))
-    })
+
+    if (iconEye){ 
+      iconEye.forEach(icon => icon.addEventListener('click', () => this.handleClickIconEye(icon))) 
+    }
+    
     new Logout({ document, localStorage, onNavigate })
+
   }
 
   handleClickNewBill = () => {
